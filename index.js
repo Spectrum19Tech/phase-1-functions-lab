e9e1cac03edea5e21cb135dea64a0888cb5540fa
Jonathan Bryant <spectrum19tech@gmail.com>
2022-01-11 00:20:12 -0800

    commit tree 51ee60bf01a5962e170272aeefc270d18c89808b

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    const distance = Math.abs(location - 42);
+    return distance;
+}
+distanceFromHqInBlocks(10);
+
+function distanceFromHqInFeet(location) {
+    const blocks = distanceFromHqInBlocks(location);
+    const feet = blocks * 264;
+    return feet;
+}
+
+function distanceTravelledInFeet(start, end) {
+    const blocks = Math.abs(start - end);
+    const feet = blocks * 264;
+    return feet;
+}
+
+function calculatesFarePrice(start, end) {
+    const distance = distanceTravelledInFeet(start, end);
+    if(distance > 2500) {
+        return 'cannot travel that far';
+    } else if(distance > 2000) {
+        return 25;
+    } else if(distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else {
+        return 0;
+    }
+}
+
+calculatesFarePrice(10,19);
